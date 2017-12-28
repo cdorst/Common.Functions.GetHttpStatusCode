e6a5da0a7c131c8b5dd738fffb686b8f3b5dde38
Christopher Dorst <chris@dorst.email>
2017-12-27 18:51:12 -0600

    Adding HttpStatusCodeGetter

---
+++ Common.Functions.GetHttpStatusCode/HttpStatusCodeGetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Common.Functions.GetHttpStatusCode
+{
+    public static class HttpStatusCodeGetter
+    {
+        public static async Task<HttpStatusCode> GetStatusCode(string uri)
+        {
+            using (var client = new HttpClient()) return (await client.GetAsync(uri)).StatusCode;
+        }
+    }
+}
